94ad947bb6c6503933aca2956f3f4ab33a8fab06
Nhan20t1020484 <99931765+Nhan20t1020484@users.noreply.github.com>
2022-03-31 22:35:45 +0700

    Add files via upload

---
+++ btlaphien12.java
@@ -0,0 +1,28 @@
+package btlapphien12;
+import java.util.Scanner;
+
+public class btlapphien12 {
+      public static void main(String[] args) {
+       Scanner bt=new Scanner (System.in);
+       try
+       {
+          for(int i=5;i>=1;i--) 
+              
+              System.out.print(i);             
+              System.out.println();
+              for(int i=4;i>=1;i--)  System.out.print(i);
+              System.out.println();
+              for(int i=3;i>=1;i--)  System.out.print(i);
+              System.out.println();
+              for(int i=2;i>=1;i--)  System.out.print(i);
+              System.out.println();
+              for(int i=1;i>=1;i--)  System.out.print(i);
+              System.out.println();
+              
+       
+       }catch(Exception e)
+     {
+          System.out.println("Loi nhap");
+     }
+}
+}
