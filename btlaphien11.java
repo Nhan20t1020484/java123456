738ecf00a09f6a796e357c6ef308b816c946e725
Nhan20t1020484 <99931765+Nhan20t1020484@users.noreply.github.com>
2022-03-31 22:35:35 +0700

    Add files via upload

---
+++ btlaphien11.java
@@ -0,0 +1,12 @@
+package btlapphien11;
+import java.util.Scanner;
+
+public class btlapphien11 {
+      public static void main(String[] args) {
+while (i<=10)
+{
+System.out.println(i);
+i++;
+}
+    }
+}
