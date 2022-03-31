fd2217b060bbc628367491897219085266a9e653
Nhan20t1020484 <99931765+Nhan20t1020484@users.noreply.github.com>
2022-03-31 22:40:32 +0700

    4

--- bt4mangvasapxep.java
+++ bt4mangvasapxep.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.Random;
 public class bt4mangvasapxep {
-    public static void b4a(int a[],int n)
+    public static void bt4mangvasapxep(int a[],int n)
     {
          Random rand=new Random();
         for (int i=0;i<n;i++)
