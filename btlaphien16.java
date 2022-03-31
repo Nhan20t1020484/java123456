bd89eebb118833ed29a0e50e6699021922bb09d5
Nhan20t1020484 <99931765+Nhan20t1020484@users.noreply.github.com>
2022-03-31 22:37:13 +0700

    Add files via upload

---
+++ btlaphien16.java
@@ -0,0 +1,41 @@
+package btlapphien16;
+import java.util.Scanner;
+
+public class btlapphien16 {
+
+  public static boolean isPrimeNumber(int n) {
+        
+        if (n < 2) {
+            return false;
+        }
+        
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    public static void main(String[] args) {
+      Scanner bt=new Scanner (System.in);
+       try{ 
+        System.out.println("Nhap n =");
+          int n=bt.nextInt();
+           for (int i=2;i<=n;i++)
+               if (isPrimeNumber(i)) 
+               {
+                   System.out.println(i);
+               }
+               
+             
+    
+} catch(Exception e)
+    {
+        System.out.println("Loi nhap!!");
+    }
+    
+    }
+}
